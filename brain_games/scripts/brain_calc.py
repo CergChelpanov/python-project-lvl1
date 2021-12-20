bdc85b55b6b218d64dbddc6a74f2c59a1eb9b6c2
CergChelpanov <chelpanov960@gmail.com>
2021-12-20 15:03:39 +0300

    worked cslc

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -7,25 +7,33 @@ print("Welcome to the Brain Games!")
 name_player = input('May I have your name?')
 print("Hello,",name_player,"!")
 
+# Повторы заданий
+
+iteration = 1
+max_limit = 3
+while iteration <= max_limit:
+
 # Задание
-print("What is the result of the expression?")
-x = random.randrange(1,100,1)
-y = random.randrange(1,100,1)
-arifmetic = random.randrange(1,4,1)
-if arifmetic == 1:
-    true_result = x + y
-    print("Question:",x,"+",y)
-if arifmetic == 2:
-    true_result = x - y
-    print("Question:",x,"-",y)
-if arifmetic == 3:
-    true_result = x * y
-    print("Question:",x,"*",y)
-result_player = input('Your answer:')
+    print("What is the result of the expression?")
+    x = random.randrange(1,100,1)
+    y = random.randrange(1,100,1)
+    arifmetic = random.randrange(1,4,1)
+    if arifmetic == 1:
+        true_result = x + y
+        print("Question:",x,"+",y)
+    if arifmetic == 2:
+        true_result = x - y
+        print("Question:",x,"-",y)
+    if arifmetic == 3:
+        true_result = x * y
+        print("Question:",x,"*",y)
+    result_player = input('Your answer:')
 
 #Результаты
-if int(true_result) - int(result_player) == 0:
-    print('Correct!')
-else:
-    print("'",result_player,"' is wrong answer. Correct answer was '", true_result,"'\nLet's try again,",name_player,"!")
+    if int(true_result) - int(result_player) == 0:
+        iteration = iteration + 1
+        print('Correct!')
+    else:
+        iteration = max_limit + 1
+        print("'",result_player,"' is wrong answer. Correct answer was '", true_result,"'\nLet's try again,",name_player,"!")
 

bd7541daa0e1929b47654e5db90de691cbe29921
CergChelpanov <chelpanov960@gmail.com>
2022-03-14 15:42:28 +0300

    14.03.22 comment23

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -15,16 +15,17 @@ def complete_the_game():
     number = randint(LOWER_BOUND + 1, UPPER_BOUND_NUM)
     x = randrange(LOWER_BOUND, number, STEP)
     y = randrange(LOWER_BOUND, number, STEP)
-    arifmetic = randrange(LOWER_BOUND, UPPER_BOUND_ARIPH, STEP)
-    if arifmetic == 1:
+# ariphmetic - эта переменная с помощью генератора случайных чисел принимает одно из трёх значений: 1, 2, 3 каждое из которых соответсвенно выбирает в задании калькулятора один из арифметических операторв: +, -, *.
+    ariphmetic = randrange(LOWER_BOUND, UPPER_BOUND_ARIPH, STEP)
+    if ariphmetic == 1:
         the_task = str(x) + ' + ' + str(y)
         correct_answer = str(x + y)
         return the_task, correct_answer
-    if arifmetic == 2:
+    if ariphmetic == 2:
         the_task = str(x) + ' - ' + str(y)
         correct_answer = str(x - y)
         return the_task, correct_answer
-    if arifmetic == 3:
+    if ariphmetic == 3:
         the_task = str(x) + ' * ' + str(y)
         correct_answer = str(x * y)
         return the_task, correct_answer

51651f0ec72f8a7775fa1d41bcd19d32ffa23244
CergChelpanov <chelpanov960@gmail.com>
2022-03-14 16:30:22 +0300

    14.03.22 comment 24

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -6,14 +6,14 @@ from random import randrange, randint
 
 RULES = 'What number is missing in the progression?'
 LOWER_BOUND = 1
-UPPER_BOUND_DELTA = 5
+UPPER_BOUND_DIFF = 5
 UPPER_BOUND_NUM = 10
 STEP = 1
 
 
-def create_progression(first_num, number_of_elements, delta):
-    return [x for x in range(first_num,
-            first_num + number_of_elements * delta, delta)]
+def create_progression(initial_term, number_of_elements, difference):
+    return [x for x in range(initial_term,
+            initial_term + number_of_elements * difference, difference)]
 
 
 def go_to_string_progression(progression, indicator):
@@ -23,10 +23,11 @@ def go_to_string_progression(progression, indicator):
 
 def complete_the_game():
     number_of_elements = 5 + randint(LOWER_BOUND, UPPER_BOUND_NUM)
-    first_num = randrange(LOWER_BOUND, number_of_elements, STEP)
-    delta = abs(randrange(LOWER_BOUND, UPPER_BOUND_DELTA, STEP))
-    progression = create_progression(first_num, number_of_elements, delta)
-    long_progression = 5 + randrange(LOWER_BOUND, UPPER_BOUND_DELTA, STEP)
+    initial_term = randrange(LOWER_BOUND, number_of_elements, STEP)
+    difference = abs(randrange(LOWER_BOUND, UPPER_BOUND_DIFF, STEP))
+    progression = create_progression(initial_term, number_of_elements,
+                                     difference)
+    long_progression = 5 + randrange(LOWER_BOUND, UPPER_BOUND_DIFF, STEP)
     joker = randrange(LOWER_BOUND + 1, int(long_progression), STEP)
     miss_num = progression[joker]
     correct_answer = str(miss_num)
